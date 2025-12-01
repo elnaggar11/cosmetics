4be710a6128fd30b4394fb24f88290dfe7d87597
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-12-01 13:34:52 +0200

    create custom text field

---
+++ lib/core/utils/ui/custom_text_field.dart
@@ -0,0 +1,59 @@
+import 'package:cosmetics/core/utils/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.keyboardType,
+    this.obscureText = false,
+  });
+  final String hintText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: const TextStyle(color: Colors.black),
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      onTapOutside: (event) {
+        FocusScope.of(context).unfocus();
+      },
+      decoration: InputDecoration(
+        labelText: hintText,
+        hintText: hintText,
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+        hintStyle: const TextStyle(
+          color: Colors.grey,
+          fontSize: 12,
+          fontWeight: FontWeight.w400,
+          fontFamily: 'segoeui',
+        ),
+        labelStyle: const TextStyle(
+          color: Colors.grey,
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          fontFamily: 'segoeui',
+        ),
+        enabledBorder: outLineInputBorder(color: Colors.grey),
+        focusedBorder: outLineInputBorder(color: AppColors.primaryColor),
+        errorBorder: outLineInputBorder(color: Colors.red),
+        focusedErrorBorder: outLineInputBorder(color: Colors.red),
+      ),
+    );
+  }
+
+  OutlineInputBorder outLineInputBorder({required Color color}) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: color),
+    );
+  }
+}
